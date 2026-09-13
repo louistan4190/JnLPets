1f22993e0f3ef7725d3e78bb933956c200b7952f
louistan4190 <louistan4190@gmail.com>
2026-09-13 10:18:24 +0800

    Update content.js

--- content.js
+++ content.js
@@ -81,7 +81,7 @@ const VALUES = [
 
 /* ---------- FACILITY GALLERY ---------- */
 const GALLERY = [
-  { image: "web.png", caption: "Grooming station" },
+  { image: "Grooming Services.jpg", caption: "Grooming station" },
   { image: "https://picsum.photos/seed/jnl-gallery-2/700/500", caption: "Cage-free boarding room" },
   { image: "https://picsum.photos/seed/jnl-gallery-3/700/500", caption: "Outdoor play yard" },
   { image: "https://picsum.photos/seed/jnl-gallery-4/700/500", caption: "Training studio" },
